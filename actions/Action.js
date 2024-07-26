17f17a904360a33d3cb2c483d114bd5faebdb3d7
Sai1Ram <sairamsenapati0022@gmail.com>
2024-07-26 17:38:45 +0530

    backend done

---
+++ actions/Action.js
@@ -0,0 +1,9 @@
+const ACTIONS = {
+    JOIN: 'JOIN',
+    LEAVE: 'LEAVE',
+    JOINED: 'JOINED',
+    USER_LEFT: 'USER_LEFT',
+    CODE_CHANGE: 'CODE_CHANGE',
+    SYNC_CODE: 'SYNC_CODE',
+}
+module.exports = ACTIONS;
---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const ACTIONS = require("./actions/Action");
+
+const app = express();
+const server = http.createServer(app);
+const PORT = process.env.PORT || 5000;
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+const users = new Map();
+const getAllUsers = (roomId) => {
+  return Array.from(io.sockets.adapter.rooms.get(roomId)).map((socketId) => {
+    return {
+      socketId,
+      userName: users.get(socketId),
+    };
+  });
+};
+io.on("connect", (socket) => {
+  socket.on(ACTIONS.JOIN, ({ roomId, userName }) => {
+    socket.join(roomId);
+    users.set(socket.id, userName);
+    const userList = getAllUsers(roomId).map((user) => user.userName);
+    // Notify all other users in the room about the new user
+    socket
+      .to(roomId)
+      .emit(ACTIONS.JOINED, { userList, userName, socketId: socket.id }); // this is for all other users except the new user
+    socket.emit(ACTIONS.JOINED, { userList, userName, socketId: socket.id }); // this is for the new user
+  });
+  socket.on(ACTIONS.LEAVE, ({ roomId, userName }) => {
+    const rooms = [...socket.rooms];
+    rooms.forEach((room) => {
+      if (room === roomId && users.get(socket.id) === userName) {
+        socket.to(room).emit(ACTIONS.USER_LEFT, userName);
+      }
+    });
+    users.delete(socket.id);
+    socket.leave(roomId);
+  });
+  socket.on(ACTIONS.CODE_CHANGE, ({ value, roomId }) => {
+    socket.to(roomId).emit(ACTIONS.CODE_CHANGE, value);
+  });
+  socket.on(ACTIONS.SYNC_CODE, ({ socketId, code }) => {
+    socket.to(socketId).emit(ACTIONS.CODE_CHANGE, code);
+  });
+});
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
